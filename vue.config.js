53e3e69c38f726430ebc657bc58abe5496433f78
kaokei <kaokei.rst@gmail.com>
2021-04-28 02:20:00 +0800

    @kaokei需要babel编译

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   lintOnSave: false,
+  transpileDependencies: ['@kaokei/post-bridge', '@kaokei/iframe-manager'],
   devServer: {
     port: 8080,
     disableHostCheck: true, // solve Invilid host header problem
